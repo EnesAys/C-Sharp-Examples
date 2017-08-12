ea09ad3e413186715cdbf5af9d500dac059441b9
Enes A. <enesaysan8@gmail.com>
2017-08-12 12:32:04 +0300

    Console01-DikdörtgenOluşturma

---
+++ DikdortgenOluşturma/DikdortgenOluşturma/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DikdortgenOluşturma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yuk, gen;
+            Console.Write("Lütfen dikdörtgen için yükseklik giriniz. ");
+            yuk = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Lütfen dikdörtgen için genişlik giriniz. ");
+            gen = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= yuk; i++)
+            {
+                for (int k = 1; k <= gen; k++)
+                {
+                   
+                    if (k == 1 || k == gen)
+                        Console.Write("*");
+                    else if(i == 1 || i == yuk)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("------------------------------------");
+
+            for (int i = 1; i <= yuk; i++)
+            {
+                for (int k = 1; k <= gen; k++)
+                {
+
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
