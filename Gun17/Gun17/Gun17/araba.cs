9c1c794a7515a3ac0113642cd99542df6ea53e30
Enes A. <enesaysan8@gmail.com>
2017-08-19 11:51:47 +0300

    Basic-class and enum defination

---
+++ Gun17/Gun17/Gun17/araba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gun17
+{
+   public class araba
+    {
+        
+        public enum tip { taksi,otobus,otomobil}
+        public enum yakitTuru { benzin,dizel,lpg}
+        public int arabaYasi { get; set; }
+        public tip ArabaninSinifi { get; set; }
+        public yakitTuru YakitTuru { get; set; }
+
+
+    }
+}
