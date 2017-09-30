2a95582661096322eea374b097e9790a6ab1411f
EnesAysan <enesaysan8@gmail.com>
2017-09-30 12:56:58 +0300

    WindowsForm-Youtube(Gazapizm35)-ChannelApi

---
+++ MiniProjects/gun26-09-17-YoutubeApi/gun26-09-17-YoutubeApi/gun26-09-17-YoutubeApi/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace gun26_09_17_YoutubeApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arama : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(196, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(339, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(61, 83);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(474, 231);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Başlık";
+            this.columnHeader1.Width = 131;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Link";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 356);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ MiniProjects/gun26-09-17-YoutubeApi/gun26-09-17-YoutubeApi/gun26-09-17-YoutubeApi/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace gun26_09_17_YoutubeApi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView1.SelectedItems.Count>0)
+            {
+                string link = listView1.SelectedItems[0].SubItems[1].Text;//Linki seçtirdik
+                System.Diagnostics.Process.Start(link);// Linki açmasını sağladık.
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            string url = string.Format("https://www.youtube.com/feeds/videos.xml?channel_id=UCi4_FLeTvoNs8cgV2cTBSWQ");
+            WebClient wc = new WebClient();//WebClient oluşturularak linkdeki xml dosyanını içerisine bilgileri indirdik 
+            string XmlData = wc.DownloadString(url);
+            XmlDocument xDoc = new XmlDocument();//xml içerisine bilgileri yükledik
+            xDoc.LoadXml(XmlData);
+            XmlNodeList girisler = xDoc.DocumentElement.GetElementsByTagName("entry");// hangi taglar arasında arayacağı
+            foreach (XmlNode item in girisler)
+            {
+                string title = item.ChildNodes[3].InnerText;
+
+                if (!string.IsNullOrEmpty(title) && title.ToLower().Contains(textBox1.Text.ToLower()))// title ve textbox kıs-
+                //mını tolover ile eşitledik  ve title contins içerisinde textbox texti varmı diye bakıyoruz.
+                {
+                    ListViewItem lv = new ListViewItem();
+                    lv.Text = title;
+                    lv.SubItems.Add(item.ChildNodes[4].Attributes["href"].InnerText);
+                    listView1.Items.Add(lv);
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            string url = string.Format("https://www.youtube.com/feeds/videos.xml?channel_id=UCi4_FLeTvoNs8cgV2cTBSWQ");
+            WebClient wc = new WebClient();//WebClient oluşturularak linkdeki xml dosyanını içerisine bilgileri indirdik 
+            string XmlData = wc.DownloadString(url);
+            XmlDocument xDoc = new XmlDocument();//xml içerisine bilgileri yükledik
+            xDoc.LoadXml(XmlData);
+            XmlNodeList girisler = xDoc.DocumentElement.GetElementsByTagName("entry");// hangi taglar arasında arayacağı
+            foreach (XmlNode item in girisler)
+            {
+                string title = item.ChildNodes[3].InnerText;
+
+                if (!string.IsNullOrEmpty(title))// t
+                {
+                    ListViewItem lv = new ListViewItem();
+                    lv.Text = title;
+                    lv.SubItems.Add(item.ChildNodes[4].Attributes["href"].InnerText);
+                    listView1.Items.Add(lv);
+                }
+
+            }
+        }
+    }
+
+}
